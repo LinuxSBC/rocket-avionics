49f296167066e0329d9133a623d6058f7b73dc6a
LinuxSBC <benedict.p.simmons@gmail.com>
2025-01-27 21:58:36 -0600

    Add SD card support

--- src/rocket-avionics.cpp
+++ src/rocket-avionics.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
 
+#include <SPI.h>
+#include "SdFat.h"
 #include <Adafruit_ICM20649.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL375.h>
 #include "Adafruit_BMP3XX.h"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define SD_CS_PIN 23
+
+#define DEBUG 1
+
+SdFat SD;
+File32 dataFile;
+SdSpiConfig config(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(16), &SPI1);
 
 Adafruit_ICM20649 icm;
 Adafruit_Sensor *icm_temp, *icm_accel, *icm_gyro;
@@ -14,24 +23,37 @@ Adafruit_ADXL375 adxl_accel = Adafruit_ADXL375(12345);
 
 Adafruit_BMP3XX bmp;
 
-bool error = false;
-
 void setup(void) {
   Serial.begin(115200);
+  #if DEBUG
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  #endif
+  
+  delay(1000);
+  
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  initSDCard();
+  initSensors();
+}
 
-  if (!icm.begin_I2C()) {
-    Serial.println("Failed to find ICM20649 chip; no gyro data");
-    error = true;
+void initSensors()
+{
+  dataFile.println("barometer altitude,barometer temperature,icm temperature,low-G accelerometer X,low-G accelerometer Y,low-G accelerometer Z,high-G accelerometer X,high-G accelerometer Y,high-G accelerometer Z,gyroscope X, gyroscope Y, gyroscope Z");
+  if (!bmp.begin_I2C())
+  {
+    error("Failed to find BMP390 chip; no barometric altitude data", false);
   }
-  if (!adxl_accel.begin()) {
-    Serial.println("Failed to find ADXL375 chip; no high-g accelerometer data");
-    error = true;
-  }
-  if (!bmp.begin_I2C()) {
-    Serial.println("Failed to find BMP390 chip; no barometric altitude data");
-    error = true;
+  
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ); // TODO: Maybe change to 200Hz?
+
+  if (!icm.begin_I2C())
+  {
+    error("Failed to find ICM20649 chip; no gyro data", false);
   }
 
   icm.setAccelRange(ICM20649_ACCEL_RANGE_30_G);
@@ -47,17 +69,39 @@ void setup(void) {
   icm_gyro = icm.getGyroSensor();
   icm_gyro->printSensorDetails();
 
+  if (!adxl_accel.begin())
+  {
+    error("Failed to find ADXL375 chip; no high-g accelerometer data", false);
+  }
+
   Serial.println("ADXL375 details:");
   adxl_accel.printSensorDetails();
-
-  // Set up oversampling and filter initialization
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ); // TODO: Maybe change to 200Hz?
 }
+void initSDCard()
+{
+  Serial.print("Initializing SD card...");
 
-void loop() {
+  // Retry mechanism for SD card initialization
+  while (!SD.begin(config))
+  {
+    error("Initialization failed! Retrying...", false);
+    delay(1000); // Wait for a second before retrying
+  }
+  Serial.println("Initialization successful.");
+
+  dataFile = SD.open("data.csv", FILE_WRITE);
+  if (!dataFile)
+  {
+    error("Failed to open data.csv on SD card");
+  }
+
+  if (!dataFile.seek(0))
+  {
+    error("Failed to seek to start of file");
+  }
+}
+void loop()
+{
   // Get a new normalized sensor event
   sensors_event_t lowg_accel;
   sensors_event_t gyro;
@@ -71,30 +115,59 @@ void loop() {
 
   // TODO: Add timestamps. What time range should I set it as? 1ms seems too long.
 
-  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA)); // TODO: Maybe set "sea level" to be the current pressure when powered on?
-  Serial.print(",");
-
-  Serial.print(bmp.temperature);
-  Serial.print(",");
-
-  Serial.print(temp.temperature);
-  Serial.print(",");
-
-  Serial.print(lowg_accel.acceleration.x);
-  Serial.print(","); Serial.print(lowg_accel.acceleration.y);
-  Serial.print(","); Serial.print(lowg_accel.acceleration.z);
-  Serial.print(",");
-
-  Serial.print(highg_accel.acceleration.x);
-  Serial.print(","); Serial.print(highg_accel.acceleration.y);
-  Serial.print(","); Serial.print(highg_accel.acceleration.z);
-  Serial.print(",");
-
-  Serial.print(gyro.gyro.x);
-  Serial.print(","); Serial.print(gyro.gyro.y);
-  Serial.print(","); Serial.print(gyro.gyro.z);
-  Serial.println();
-
+  if (dataFile) {
+    printSensorsToFile(temp, lowg_accel, highg_accel, gyro);
+  }
 
   delay(10);
+
+  // TODO: Find a way to close the file
+  if (digitalRead(PIN_BUTTON) == LOW && dataFile) {
+    Serial.println("Closing file");
+    dataFile.close();
+  }
+}
+
+void printSensorsToFile(sensors_event_t &temp, sensors_event_t &lowg_accel, sensors_event_t &highg_accel, sensors_event_t &gyro)
+{
+  dataFile.print(bmp.readAltitude(SEALEVELPRESSURE_HPA)); // TODO: Maybe set "sea level" to be the current pressure when powered on?
+
+  dataFile.print(",");
+  dataFile.print(bmp.temperature);
+
+  dataFile.print(",");
+  dataFile.print(temp.temperature);
+
+  dataFile.print(",");
+  dataFile.print(lowg_accel.acceleration.x);
+  dataFile.print(",");
+  dataFile.print(lowg_accel.acceleration.y);
+  dataFile.print(",");
+  dataFile.print(lowg_accel.acceleration.z);
+
+  dataFile.print(",");
+  dataFile.print(highg_accel.acceleration.x);
+  dataFile.print(",");
+  dataFile.print(highg_accel.acceleration.y);
+  dataFile.print(",");
+  dataFile.print(highg_accel.acceleration.z);
+
+  dataFile.print(",");
+  dataFile.print(gyro.gyro.x);
+  dataFile.print(",");
+  dataFile.print(gyro.gyro.y);
+  dataFile.print(",");
+  dataFile.print(gyro.gyro.z);
+
+  dataFile.println();
+}
+
+void error(String message, bool fatal = true) {
+  Serial.println(message);
+  if (fatal) {
+    dataFile.close();
+    while (1) {
+      yield();
+    }
+  }
 }
