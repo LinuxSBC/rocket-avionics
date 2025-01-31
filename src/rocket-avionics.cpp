b539009cf2f0004eb7f6ab11138b3fc4762438af
LinuxSBC <benedict.p.simmons@gmail.com>
2025-01-30 22:37:51 -0600

    Add NeoPixel indicator; more refactoring

--- src/rocket-avionics.cpp
+++ src/rocket-avionics.cpp
@@ -6,11 +6,17 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL375.h>
 #include "Adafruit_BMP3XX.h"
+#include <Adafruit_NeoPixel.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define SD_CS_PIN 23
 
-#define DEBUG 1
+#define DEBUG 0
+
+void initSensors();
+void initSDCard();
+void printSensorsToFile(sensors_event_t &temp, sensors_event_t &lowg_accel, sensors_event_t &highg_accel, sensors_event_t &gyro);
+void error(String message, bool fatal = true);
 
 SdFat SD;
 File32 dataFile;
@@ -23,7 +29,13 @@ Adafruit_ADXL375 adxl_accel = Adafruit_ADXL375(12345);
 
 Adafruit_BMP3XX bmp;
 
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, PIN_NEOPIXEL);
+
 void setup(void) {
+  pixel.begin();
+  pixel.setPixelColor(0, pixel.Color(0, 0, 255));
+  pixel.show();
+
   Serial.begin(115200);
   #if DEBUG
   while (!Serial)
@@ -35,71 +47,11 @@ void setup(void) {
   pinMode(PIN_BUTTON, INPUT_PULLUP);
   initSDCard();
   initSensors();
+
+  pixel.setPixelColor(0, pixel.Color(0, 255, 0));
+  pixel.show();
 }
 
-void initSensors()
-{
-  dataFile.println("barometer altitude,barometer temperature,icm temperature,low-G accelerometer X,low-G accelerometer Y,low-G accelerometer Z,high-G accelerometer X,high-G accelerometer Y,high-G accelerometer Z,gyroscope X, gyroscope Y, gyroscope Z");
-  if (!bmp.begin_I2C())
-  {
-    error("Failed to find BMP390 chip; no barometric altitude data", false);
-  }
-  
-  // Set up oversampling and filter initialization
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ); // TODO: Maybe change to 200Hz?
-
-  if (!icm.begin_I2C())
-  {
-    error("Failed to find ICM20649 chip; no gyro data", false);
-  }
-
-  icm.setAccelRange(ICM20649_ACCEL_RANGE_30_G);
-  icm.setGyroRange(ICM20649_GYRO_RANGE_2000_DPS);
-
-  Serial.println("ICM20649 details:");
-  icm_temp = icm.getTemperatureSensor();
-  icm_temp->printSensorDetails();
-
-  icm_accel = icm.getAccelerometerSensor();
-  icm_accel->printSensorDetails();
-
-  icm_gyro = icm.getGyroSensor();
-  icm_gyro->printSensorDetails();
-
-  if (!adxl_accel.begin())
-  {
-    error("Failed to find ADXL375 chip; no high-g accelerometer data", false);
-  }
-
-  Serial.println("ADXL375 details:");
-  adxl_accel.printSensorDetails();
-}
-void initSDCard()
-{
-  Serial.print("Initializing SD card...");
-
-  // Retry mechanism for SD card initialization
-  while (!SD.begin(config))
-  {
-    error("Initialization failed! Retrying...", false);
-    delay(1000); // Wait for a second before retrying
-  }
-  Serial.println("Initialization successful.");
-
-  dataFile = SD.open("data.csv", FILE_WRITE);
-  if (!dataFile)
-  {
-    error("Failed to open data.csv on SD card");
-  }
-
-  if (!dataFile.seek(0))
-  {
-    error("Failed to seek to start of file");
-  }
-}
 void loop()
 {
   // Get a new normalized sensor event
@@ -125,6 +77,85 @@ void loop()
   if (digitalRead(PIN_BUTTON) == LOW && dataFile) {
     Serial.println("Closing file");
     dataFile.close();
+    pixel.setPixelColor(0, pixel.Color(0, 255, 255));
+    pixel.show();
+    while (1) {
+      yield();
+    }
+  }
+}
+
+void initSensors()
+{
+  dataFile.println("barometer altitude,barometer temperature,icm temperature,low-G accelerometer X,low-G accelerometer Y,low-G accelerometer Z,high-G accelerometer X,high-G accelerometer Y,high-G accelerometer Z,gyroscope X, gyroscope Y, gyroscope Z");
+
+  if (!bmp.begin_I2C())
+  {
+    error("Failed to find BMP390 chip; no barometric altitude data", false);
+  }
+  
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_200_HZ); // TODO: Maybe change to 200Hz?
+
+  if (!icm.begin_I2C())
+  {
+    error("Failed to find ICM20649 chip; no gyro data", false);
+  }
+
+  icm.setAccelRange(ICM20649_ACCEL_RANGE_30_G);
+  icm.setGyroRange(ICM20649_GYRO_RANGE_4000_DPS);
+
+  icm_temp = icm.getTemperatureSensor();
+  icm_accel = icm.getAccelerometerSensor();
+  icm_gyro = icm.getGyroSensor();
+
+  if (!adxl_accel.begin())
+  {
+    error("Failed to find ADXL375 chip; no high-g accelerometer data", false);
+  }
+
+  #if DEBUG
+  Serial.println("BMP390 details:");
+  bmp.printSensorDetails();
+
+  Serial.println("ICM20649 details:");
+  icm_temp->printSensorDetails();
+  icm_accel->printSensorDetails();
+  icm_gyro->printSensorDetails();
+
+  Serial.println("ADXL375 details:");
+  adxl_accel.printSensorDetails();
+  #endif
+}
+
+void initSDCard()
+{
+  #if DEBUG
+  Serial.print("Initializing SD card...");
+  #endif
+
+  // Retry mechanism for SD card initialization
+  while (!SD.begin(config))
+  {
+    error("Initialization failed! Retrying...", false);
+    delay(1000); // Wait for a second before retrying
+  }
+  #if DEBUG
+  Serial.println("Initialization successful.");
+  #endif
+
+  dataFile = SD.open("data.csv", FILE_WRITE);
+  if (!dataFile)
+  {
+    error("Failed to open data.csv on SD card");
+  }
+
+  if (!dataFile.seek(0))
+  {
+    error("Failed to seek to start of file");
   }
 }
 
@@ -162,12 +193,18 @@ void printSensorsToFile(sensors_event_t &temp, sensors_event_t &lowg_accel, sens
   dataFile.println();
 }
 
-void error(String message, bool fatal = true) {
+void error(String message, bool fatal) {
   Serial.println(message);
   if (fatal) {
+    pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+    pixel.show();
     dataFile.close();
     while (1) {
       yield();
     }
+  }
+  else {
+    pixel.setPixelColor(0, pixel.Color(255, 120, 0));
+    pixel.show();
   }
 }
